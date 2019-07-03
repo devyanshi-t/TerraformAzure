d4ee4af8de5e2a88ce7d7bfe39bba6bae1aca297
Devyanshi Tiwari <devyanshi12aug@gmail.com>
2019-07-03 12:04:23 +0530

    Arm template using terraform

---
+++ ARMTemplateUsingTerraform/ReadMe.md
@@ -0,0 +1,29 @@
+
+# Deploying  an ARM template  using Terraform in Azure
+Terraform provides an easy way to define, preview, and deploy cloud infrastructure by using a simple templating language. 
+In this example an ARM template is deployed using terraform.
+
+## Steps:<br/>
+
+- On the Azure CLI type the commands
+    
+    ```az account set --subscription="${SUBSCRIPTION_ID}"
+    az ad sp create-for-rbac --role="Contributor" --scopes="/subscriptions/${SUBSCRIPTION_ID}"
+
+    echo "Setting environment variables for Terraform"
+    export ARM_SUBSCRIPTION_ID=your_subscription_id
+    export ARM_CLIENT_ID=your_appId
+    export ARM_CLIENT_SECRET=your_password
+    export ARM_TENANT_ID=your_tenant_id
+    export ARM_ENVIRONMENT=public```
+
+
+- Use the .tf file <br/>
+
+### To Deploy
+```bash 
+terraform init
+terraform plan
+terraform apply
+```
+
---
+++ ARMTemplateUsingTerraform/terra.tf
@@ -0,0 +1,52 @@
+
+resource "azurerm_resource_group" "test" {
+  name     = "myResourceGroup"
+  location = "East US"
+
+
+}
+
+resource "azurerm_template_deployment" "test" {
+  name                = "Main-Deploy-2"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+
+  template_body = "${file("./master.json")}"
+
+  parameters={
+      "Vnet1name"="virtualnetwork1"
+      "location"="westus",
+      "VnetaddressPrefix"="10.0.0.0/16",
+      "subnetName1"="S1",
+       "subnetName2"="S2",
+      "subnetName3"="S3",
+       "firewallSubnetAddressPrefix"="10.0.1.0/24",
+      "subnetAddressPrefix1"="10.0.2.0/24",
+      "subnetAddressPrefix2"="10.0.3.0/24",
+      "subnetAddressPrefix3"="10.0.4.0/24",
+      "SecondVnetname"="virtualnetwork2"
+       "Secondlocation"="eastus",
+      "SecondVnetaddressPrefix"="20.0.0.0/16",
+      "SecondVnetsubnetName1"="S21",
+      "SecondVnetsubnetName2"="S22",
+       "SecondVnetsubnetName3"="S23",
+       "SecondVnetsubnetAddressPrefix1"="20.0.2.0/24",
+        "SecondVnetsubnetAddressPrefix2"="20.0.3.0/24",
+         "SecondVnetsubnetAddressPrefix3"="20.0.4.0/24",
+          "SecondVnetfirewallSubnetAddressPrefix"="20.0.1.0/24",
+           "Web_NSGName"="WEB1",
+             "Api_NSGName"="AP1",
+             "Db_NSGName"="DB1",
+             "Web1_NSGName"="WEB2",
+             "Api1_NSGName"="AP2",
+              "Db1_NSGName"="DB2",
+      "enableDdosProtection"=false,
+     "virtualNetworkAccess"=true,
+      "forwardedTraffic"=false,
+      "gatewayTransit"=false,
+      "remoteGateways"=false
+
+  }
+  deployment_mode="Incremental"
+
+}
+
