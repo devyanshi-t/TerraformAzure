19ffd0128463bbc3c7435cbc10835aae09243a55
Devyanshi Tiwari <devyanshi12aug@gmail.com>
2019-07-18 16:31:48 +0530

    Added dual disaster recovery rules

--- THREE_TIER_ARCHITECTURE/powershell.sh
+++ THREE_TIER_ARCHITECTURE/powershell.sh
@@ -7,6 +7,8 @@ $customport=3308 # add your own custom port
 
 $rule1="DisasterRecoveryRule1"
 $rule2="DisasterRecoveryRule2"
+$rule3="DisasterRecoveryRule3"
+$rule4="DisasterRecoveryRule4"
 
 $nsgname1="databaseterra1" #Change the Name according to need
 $nsgname2="databaseterra2"
@@ -16,15 +18,26 @@ $nsg1 = Get-AzNetworkSecurityGroup -Name $nsgname1 -ResourceGroupName $ResourceG
 $nsg2 = Get-AzNetworkSecurityGroup -Name $nsgname2 -ResourceGroupName $ResourceGroupName
 
 # Add the inbound security rule.
-$nsg2 | Add-AzNetworkSecurityRuleConfig -Name $rule1 -Description "Allow Inbound Coomunication from databse subnet 2 to to database subnet 1" -Access Allow `
+$nsg2 | Add-AzNetworkSecurityRuleConfig -Name $rule1 -Description "Allow  Coomunication from databse subnet 1 to to database subnet 2" -Access Allow `
+-Protocol * -Direction Inbound -Priority 3000 -SourceAddressPrefix "10.0.4.0/24" -SourcePortRange $customport `
+-DestinationAddressPrefix "20.0.4.0/24" -DestinationPortRange $customport
+
+# Add the outbound security rule.
+$nsg1 | Add-AzNetworkSecurityRuleConfig -Name $rule2 -Description "Allow Communication from databse subnet 1 to to database subnet 2" -Access Allow `
+-Protocol * -Direction Outbound -Priority 3000 -SourceAddressPrefix  10.0.4.0/24" -SourcePortRange $customport `
+-DestinationAddressPrefix "20.0.4.0/24" -DestinationPortRange $customport
+
+$nsg1| Add-AzNetworkSecurityRuleConfig -Name $rule3 -Description "Allow  Communication from databse subnet 2 to to database subnet 1" -Access Allow `
 -Protocol * -Direction Inbound -Priority 3000 -SourceAddressPrefix "20.0.4.0/24" -SourcePortRange $customport `
 -DestinationAddressPrefix "10.0.4.0/24" -DestinationPortRange $customport
 
 
-# Add the outbound security rule.
-$nsg1 | Add-AzNetworkSecurityRuleConfig -Name $rule2 -Description "Allow otbound Communication from databse subnet 2 to to database subnet 1" -Access Allow `
--Protocol * -Direction Outbound -Priority 3000 -SourceAddressPrefix "10.0.4.0/24" -SourcePortRange $customport `
--DestinationAddressPrefix "20.0.4.0/24" -DestinationPortRange $customport
+$nsg2 | Add-AzNetworkSecurityRuleConfig -Name $rule4 -Description "Allow  Communication from databse subnet 2 to to database subnet 1" -Access Allow `
+-Protocol * -Direction outbound -Priority 3000 -SourceAddressPrefix "20.0.4.0/24" -SourcePortRange $customport `
+-DestinationAddressPrefix "10.0.4.0/24" -DestinationPortRange $customport
+
+
+
 
 # Update the NSG.
 $nsg1 | Set-AzNetworkSecurityGroup
