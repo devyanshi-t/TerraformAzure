076bb859a6d77908a0d11d9b8e8e03d387219699
Devyanshi Tiwari <devyanshi12aug@gmail.com>
2019-07-19 18:37:02 +0530

    changes in the.tf file

--- ARMTemplateUsingTerraform/terra.tf
+++ ARMTemplateUsingTerraform/terra.tf
@@ -7,7 +7,7 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_template_deployment" "test" {
-  name                = "Main-Deploy-2"
+  name                = "testdeploy"
   resource_group_name = "${azurerm_resource_group.test.name}"
 
   template_body = "${file("./master.json")}"
